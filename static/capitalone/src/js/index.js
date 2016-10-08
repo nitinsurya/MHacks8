d86b53fe36aa270b9cafa5b2e95dbff40f40df09
nitinsurya <nitinsurya1991@gmail.com>
2016-10-08 15:37:43 -0500

    ui updates

--- static/capitalone/src/js/index.js
+++ static/capitalone/src/js/index.js
@@ -1,26 +1,26 @@
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-var data = google.visualization.arrayToDataTable([
-  ['category', 'amount'],
-  ['Food', 4],
-  ['Travel', 2],
-  ['Entertainment', 2]
-]);
+// google.charts.load("current", {packages:["corechart"]});
+// google.charts.setOnLoadCallback(drawChart);
+// function drawChart() {
+//   var data = google.visualization.arrayToDataTable([
+//     ['category', 'amount'],
+//     ['Food', 4],
+//     ['Travel', 2],
+//     ['Entertainment', 2]
+//   ]);
 
-var options = {
-  title: 'Spend Summary',
-  pieHole: 0.4,
-  colors: ['#009688','#ff5722','#9c27b0']
-};
+//   var options = {
+//     title: 'Spend Summary',
+//     pieHole: 0.4,
+//     colors: ['#009688','#ff5722','#9c27b0']
+//   };
 
-var chart = new google.visualization.PieChart($.('x_panel_pie'));
-chart.draw(data, options);
-}
+//   var chart = new google.visualization.PieChart($('x_panel_pie'));
+//   chart.draw(data, options);
+// }
 
 $(document).ready(function() {
 
-		$.('x_panel_calender').fullCalendar({
+		$('x_panel_calender').fullCalendar({
 		// allow "more" link when too many events
 			events: './events.json'
 		});
@@ -28,16 +28,16 @@ $(document).ready(function() {
 
 var map;
 function initMap(){
- map = new google.maps.Map($.('x_panel_map')), {
-  zoom: 10,
-  center: new google.maps.LatLng(41.859483,-88.0598467),
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-
-});
-setmarkers();
+   map = new google.maps.Map($('.x_panel_map'), {
+    zoom: 10,
+    center: new google.maps.LatLng(41.859483,-88.0598467),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  setmarkers();
 }
-function setmarkers()
-{
+
+
+function setmarkers() {
   $.getJSON("http://52.77.240.18:8081/app_content?coords=true",function(data){
     console.log(data);
      data.transactions.forEach(function(d) {
