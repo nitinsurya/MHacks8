6a7570adc1d6525250eb4bbebc2e060ca81eb48d
nitinsurya <nitinsurya1991@gmail.com>
2016-10-08 20:52:18 -0500

    Cleaned up the failing assets, streamlined the backend

--- api.py
+++ api.py
@@ -38,46 +38,44 @@ def app_content():
   req = requests.get(url)
   if req.status_code == 200:
 
-    json_data = req.json()
-    out_vals = {'curr_bal': {'val': str(10000 - json_data['balance']), 'text-sub': "Due on Nov 6"},
-        'credit': {'val': str(json_data['balance']), 'text-sub': "Credit limit: $10000"}}
+    json_data1 = req.json()
     url = "http://api.reimaginebanking.com/accounts/" + account_id + "/purchases?key=" + mhack_key
-    req = requests.get(url)
-    json_data = req.json()
+    json_data = requests.get(url).json()
     
     for elem in json_data:
       merchant_details = merchant_data[elem['merchant_id']]
       if coords:
-        out_vals['transactions'] = [{'name': merchant_details['name'], 'date': get_format_data(elem['purchase_date']), 'amount': str(elem['amount']),
+        out_vals = {'curr_bal': {'val': str(round(10000 - json_data1['balance'], 2)), 'text-sub': "Nov 6"},
+          'credit': {'val': str(json_data1['balance']), 'text-sub': "$10000"}}
+        out_vals['transactions'] = [{'name': merchant_details['name'], 'date': get_format_date(elem['purchase_date']), 'amount': str(elem['amount']),
                   'lat': merchant_details['lat'], 'lon': merchant_details['lng']},
                 {'name': 'Chapati Indian Grill', 'date': 'SATURDAY, OCT 8', 'amount': "$44.05", 'lat': 41.879483, 'lon': -88.1098467}]
       else:
-        out_vals['transactions'] = [{'name': merchant_details['name'], 'date': get_format_data(elem['purchase_date']), 'amount': str(elem['amount'])},
+        out_vals = {'curr_bal': {'val': round(10000 - json_data1['balance'], 2), 'text-sub': "Due on Nov 6"},
+          'credit': {'val': str(json_data1['balance']), 'text-sub': "Credit limit: $10000"}}
+        out_vals['transactions'] = [{'name': merchant_details['name'], 'date': get_format_date(elem['purchase_date']), 'amount': str(elem['amount'])},
                             {'name': 'Chapati Indian Grill', 'date': 'SATURDAY, OCT 8', 'amount': "$44.05"}]
   else:
     return make_response(jsonify({"error": "Something went wrong"}))
 
   url = "http://api.reimaginebanking.com/accounts/" + account_id + "/bills?key=" + mhack_key
   req = requests.get(url).json()
-  out_vals['subscriptions'] = [{'name': 'Spotify', 'date': 'Scheduled on Oct 8', 'amount': "$9.99"},
-                        {'name': 'Google Express', 'date': 'Scheduled on Oct 14', 'amount': "$10.00"},
-                        {'name': 'LinkedIn Subscription', 'date': 'Scheduled on Oct 16', 'amount': "$29.99"}]
+  out_vals['subscriptions'] = get_subscription_data()
+  out_vals['pie_content'] = get_pie_content()
   return make_response(jsonify(out_vals))
 
 @app.route('/app_events', methods=['GET'])
 def app_events():
-  out_vals = [{
-                "title": "event1",
-                "start": "2016-10-20"
-              }, {
-                "title": "event2",
-                "start": "2016-10-05",
-                "end": "2016-10-07"
-              }, {
-                "title": "event3",
-                "start": "2016-10-09T12:30:00",
-                "allDay": False
-              }]
+  account_id, out_vals = '57f89267360f81f104543bd1', []
+  url = "http://api.reimaginebanking.com/accounts/" + account_id + "/purchases?key=" + mhack_key
+  json_data = requests.get(url).json()
+  
+  for elem in json_data:
+    out_vals.append({"title": merchant_data[elem['merchant_id']]['name'] + " $" + str(elem['amount']), "start": elem['purchase_date'], "color": "blue"})
+
+  for elem in get_subscription_data():
+    out_vals.append({"title": elem['name'] + " $" + elem['amount'], 'start': elem['start_date'], "color": "red"})
+
   return make_response(jsonify(out_vals))
 
 @app.route('/mhacks', methods=['GET'])
@@ -94,9 +92,16 @@ def mhacks():
 def not_found(error):
   return make_response(jsonify({'error': 'Not found'}), 404)
 
+def get_subscription_data():
+  return [{'name': 'Spotify', 'date': 'Scheduled on Oct 8', 'start_date': "2016-10-08", 'amount': "$9.99"},
+                        {'name': 'Google Express', 'date': 'Scheduled on Oct 14', 'start_date': "2016-10-14", 'amount': "$10.00"},
+                        {'name': 'LinkedIn Subscription', 'date': 'Scheduled on Oct 16', 'start_date': "2016-10-16", 'amount': "$29.99"}]
 
-def get_format_data(date_str):
+def get_format_date(date_str):
   return date_str
+
+def get_pie_content():
+  return [['category', 'amount'], ['Food', 4], ['Travel', 2], ['Entertainment', 2]]
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8081, debug=True, threaded=True)
--- static/capitalone/src/js/index.js
+++ static/capitalone/src/js/index.js
@@ -1,53 +1,84 @@
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-var server_ip = '0.0.0.0:8081'
-function drawChart() {
-  console.log('hello there');
-  var data = google.visualization.arrayToDataTable([
-    ['category', 'amount'],
-    ['Food', 4],
-    ['Travel', 2],
-    ['Entertainment', 2]
-  ]);
+function load_charts() {
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
+}
+server_ip = "0.0.0.0:8081"
 
-  var options = {
-    title: 'Spend Summary',
-    pieHole: 0.4,
-    colors: ['#009688','#ff5722','#9c27b0']
-  };
-
-  var chart = new google.visualization.PieChart($('.x_panel_pie')[0]);
-  chart.draw(data, options);
+data = {
+  content: null
 }
 
+function drawChart() {
+  var dataC = google.visualization.arrayToDataTable(data.content.pie_content);
+
+  var options = {
+    pieHole: 0.4,
+    colors: ['#3F51B5','#E8700C','#8BC34A'],
+    backgroundColor: 'transparent',
+    legend: {
+  	  alignment: 'center'
+    },
+    tooltip: {
+  	  text: 'percentage'
+    }
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  chart.draw(dataC, options);
+}
+
+
 $(document).ready(function() {
-		$('.x_panel_calendar').fullCalendar({
-		// allow "more" link when too many events
-			events: 'http://52.77.240.18:8081/app_events'
-		});
+  // $.getJSON('http://' + server_ip + '/app_events', function(data) {
+    $('.x_panel_calendar').fullCalendar({
+      // allow "more" link when too many events
+      events: 'http://' + server_ip + '/app_events'
+    });
+  // })
 });
 
 var map;
-function initMap(){
-   map = new google.maps.Map($('#world-map-gdp')[0], {
-    zoom: 10,
-    center: new google.maps.LatLng(41.859483,-88.0598467),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+function initMap() {
+  $.getJSON("http://" + server_ip + "/app_content?coords=true", function(d) {
+    data.content = d;
+    map = new google.maps.Map($('#world-map-gdp')[0], {
+      zoom: 10,
+      center: new google.maps.LatLng(41.859483,-88.0598467),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    setmarkers(d);
+    updateBalanceDueCredit(d);
+    addTransactionsToList($(".x_panel.transactions ul").last(), d.transactions, '');
+    load_charts();
   });
-  setmarkers();
 }
 
+function updateBalanceDueCredit(data) {
+  $($(".row.tile_count .tile_stats_count .count").get(0)).html(data.curr_bal.val)
+  $($(".row.tile_count .tile_stats_count .count").get(1)).html(data.curr_bal['text-sub'])
+  $($(".row.tile_count .tile_stats_count .count").get(2)).html(data.credit.val)
+  $($(".row.tile_count .tile_stats_count .count").get(3)).html(data.credit['text-sub'])
+}
 
-function setmarkers() {
-  $.getJSON("http://" + server_ip + "/app_content?coords=true",function(data){
-     data.transactions.forEach(function(d) {
-       console.log(d);
-       var marker = new google.maps.Marker({
-           position:new google.maps.LatLng(d.lat, d.lon) ,
-           map: map,
-           title: d.name
-         });
-      });
+function addTransactionsToList(dom_object, transactions, additional_class) {
+    var count = 0;
+    $.each(transactions, function(index, transaction) {
+      count += 1;
+      $(dom_object).append('<li>' + 
+          '<div class="name">' + transaction.name + '</div>' +
+          '<div class="timestamp">' + transaction.date + '</div>' +
+          '<div class="amount ' + (transaction.amount[0] == '-' ? 'negative' : '') + additional_class + '">' + transaction.amount + '</div>' +
+          '</li>')
     });
+}
+
+function setmarkers(data) {
+  data.transactions.forEach(function(d) {
+   var marker = new google.maps.Marker({
+       position:new google.maps.LatLng(d.lat, d.lon) ,
+       map: map,
+       title: d.name
+     });
+  });
 }
  
